adc452f6810bff8d77021e3875151345334987c5
namho.kim <shawnbest@naver.com>
2022-12-04 02:37:14 +0900

    Radar graph with dynamic data

--- router/main.js
+++ router/main.js
@@ -5,7 +5,17 @@ module.exports = function(app){
     });
     app.get('/search/:singer',function(req, res, next){
         singer = req.params.singer;
+        
+        var data = [1,2.4,3.6,4,5,7];
 
-        res.render('list', {title: req.params.singer, singer: singer})
+        res.render('list', {title: req.params.singer, 
+            singer: singer, 
+            data0:data[0], 
+            data1:data[1], 
+            data2:data[2],
+            data3:data[3],
+            data4:data[4],
+            data5:data[5]
+        })
     })
 }
