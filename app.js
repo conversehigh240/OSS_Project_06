f89cd5eebf747802e7de941465ed58a1b6e01fc6
jiho_kang <init.jiho9487@gmail.com>
2022-12-06 22:08:48 +0900

    Link Front app.js and AWS

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var router = require('./router/main')(app);
+var bodyParser = require('body-parser');
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.json());	// json 등록
+app.use(bodyParser.urlencoded({ extended : false }));
+
+var server = app.listen(8888, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log('listening on 8888', host, port)
+});
